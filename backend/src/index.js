cad762ce2e3429e20875cda4be30dd4695fc8633
Christian Yitzak <christianyitzak@gmail.com>
2026-09-13 10:58:23 +0700

    setup database connection and routes placeholder

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,15 @@
 import express from "express";
 import "dotenv/config";
 
+import authRoutes from "./routes/authRoutes.js";
+import { connectDB } from "./lib/db.js";
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use("/api/auth", authRoutes);
+
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
+  connectDB();
 });
