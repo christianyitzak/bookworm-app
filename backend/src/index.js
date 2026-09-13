cb4f98d8ff7e894f67cbe50ded0322d8645b30f7
Christian Yitzak <christianyitzak@gmail.com>
2026-09-13 10:43:48 +0700

    setup backend

---
+++ backend/src/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
+});
